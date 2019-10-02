893f8e65593b25fb2fff7928096588aed7ebed7d
unknown <hj20409@gmail.com>
2019-10-02 11:08:35 +0800

    first commit

---
+++ test_p16/src/test_p16/test_p16.java
@@ -0,0 +1,13 @@
+package test_p16;
+
+public class test_p16 {
+
+	public static void main(String[] args) {
+		System.out.println("A");
+		System.out.println("開始使用Java!");
+		System.out.println(123);
+		// TODO Auto-generated method stub
+
+	}
+
+}
